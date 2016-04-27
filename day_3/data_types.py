f9caa7c577f4b572d2a6ac7649a3483e17903a08
Gladys <gladysmumbua@gmail.com>
2016-04-27 00:40:09 -0700

    data_type Function

---
+++ day_3/data_types.py
@@ -0,0 +1,35 @@
+def data_type(x):
+	'''
+	Takes in an argument, x:
+		- For an integer, return x ** 2
+		- For a float, return x / 2
+		- For a string, return "hello" + x
+		- For a boolean, return "boolean"
+		- For a long, return squareroot (x ** 0.5)
+	'''
+	
+
+	if type (x) == int:
+		return x ** 2
+
+	elif type (x) == float:
+		return x / 2
+
+	elif type (x) == str:
+		return "Hello" + " "  + x
+
+	elif type (x) == bool:
+		return "Boolean"
+
+	elif type (x) == long:
+		return "Long"
+	else:
+		print "Invalid data type"
+
+
+print data_type (2)
+print data_type (3.14)
+print data_type ("World")
+print data_type (True)
+print data_type (35 ** 45)
+print data_type ([1,2,2])
